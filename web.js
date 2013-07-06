0c17bb2479590ac01e5a531fc47596d6f3c6b220
ashishbhutani <bhutani.ashish14@gmail.com>
2013-07-06 20:28:59 +0530

    update web.js nd index.html

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buf = new Buffer()
+  buf = fs.readFileSync('index.html')
+  var str = buf.toString();
+  console.log(str)
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listeningn " + port);
 });
